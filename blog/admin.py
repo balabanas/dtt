048a877ea68337037d01e75d334cf3b496a775f3
Anton Balabanov <asbbnv@gmail.com>
2023-11-20 12:35:36 +0200

    register Article in admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import Article
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ("title", "snippet", "pub_dttm", "author", "online")
+    prepopulated_fields = {"slug": ("title", )}
+
+
+admin.site.register(Article, ArticleAdmin)
