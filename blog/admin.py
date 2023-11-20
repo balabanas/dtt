ff2c216fd9e0202a2d6f086b262da373eaa27494
Anton Balabanov <asbbnv@gmail.com>
2023-11-20 12:37:48 +0200

    register ContactRequest in admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 
-from blog.models import Article
+from blog.models import Article, ContactRequest
 
 
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ("title", "snippet", "pub_dttm", "author", "online")
-    prepopulated_fields = {"slug": ("title", )}
+    prepopulated_fields = {"slug": ("title",)}
+
+
+class ContactRequestAdmin(admin.ModelAdmin):
+    list_display = ("name", "email", "pub_dttm",)
 
 
 admin.site.register(Article, ArticleAdmin)
+admin.site.register(ContactRequest, ContactRequestAdmin)
