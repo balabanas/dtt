3fb17eb06db9d8375e6acf18b86326fc39c08684
Anton Balabanov <asbbnv@gmail.com>
2023-11-20 15:05:41 +0200

    ensure full name for author in ArticleDetailView, with fallback to username

--- blog/tests/test_view_article_detail.py
+++ blog/tests/test_view_article_detail.py
@@ -8,11 +8,14 @@ from blog.models import Article
 
 class TestArticleDetailView(TestCase):
     def setUp(self) -> None:
-        self.user: User = User.objects.create(username="testuser", password="12345")
+        self.user: User = User.objects.create(username="testuser")
+        self.fullname_user = User.objects.create(username="testuser2", first_name="Name", last_name="Lastname")
         self.aoff: Article = Article.objects.create(title="Offline test title", content="test content",
                                                     author=self.user)
         self.aon: Article = Article.objects.create(title="Online test title", content="test content", online=True,
                                                    author=self.user)
+        self.aon_fu = Article.objects.create(title="Online test title", slug="test-title-online",
+                                             content="test content", online=True, author=self.fullname_user)
 
     def test_article_detail_view_online(self) -> None:
         response: TemplateResponse = self.client.get(
@@ -27,3 +30,10 @@ class TestArticleDetailView(TestCase):
         response: TemplateResponse = self.client.get(
             reverse('article-detail', kwargs={'id': self.aoff.id, 'slug': self.aoff.slug}))
         self.assertEqual(response.status_code, 404)
+
+    def test_article_detail_view_display_user_fullname_with_username_fallback(self):
+        response = self.client.get(reverse('article-detail', kwargs={'id': self.aon_fu.id, 'slug': self.aon_fu.slug}))
+        self.assertContains(response, self.fullname_user.get_full_name())
+        self.assertNotContains(response, self.fullname_user.username)
+        response = self.client.get(reverse('article-detail', kwargs={'id': self.aon.id, 'slug': self.aon.slug}))
+        self.assertContains(response, self.user.username)
