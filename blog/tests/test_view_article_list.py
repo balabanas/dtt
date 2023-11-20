6a50278bad680307b0aff5896edb94cccd3ec0e3
Anton Balabanov <asbbnv@gmail.com>
2023-11-20 14:42:30 +0200

    add pagination + tests

--- blog/tests/test_view_article_list.py
+++ blog/tests/test_view_article_list.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 from django.urls import reverse
 
 from blog.models import Article
+from blog.views import ArticleListView
 
 
 class TestArticleListView(TestCase):
@@ -21,3 +22,28 @@ class TestArticleListView(TestCase):
         self.assertContains(response, 'Online test title')
         self.assertContains(response, 'test content...')
         self.assertContains(response, self.user.username)
+
+
+class ArticlePaginationTestCase(TestCase):
+    def setUp(self):
+        user = User.objects.create(username="testuser", password="12345")
+        for i in range(14):
+            Article.objects.create(title=f"Online test title {i + 1}", slug=f"test-title- {i + 1}",
+                                   content=f"test content {i + 1}", online=True, author=user)
+
+    def test_pagination_first_page(self):
+        response = self.client.get(reverse('blog-home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'blog/article_list.html')
+        self.assertTrue('page_obj' in response.context)
+        self.assertEqual(len(response.context['page_obj']), ArticleListView.paginate_by)
+
+    def test_pagination_second_page(self):
+        response = self.client.get(reverse('blog-home') + '?page=2')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('page_obj' in response.context)
+        self.assertEqual(len(response.context['page_obj']), ArticleListView.paginate_by)
+
+    def test_invalid_page_number(self):
+        response = self.client.get(reverse('blog-home') + '?page=invalid')
+        self.assertEqual(response.status_code, 404)
--- blog/views.py
+++ blog/views.py
@@ -2,11 +2,14 @@ from django.views.generic import ListView, DetailView
 
 from blog.models import Article
 
+ARTICLE_PAGINATION_SIZE = 5
+
 
 class ArticleListView(ListView):
     model = Article
     template_name = 'blog/article_list.html'
     context_object_name = 'articles'
+    paginate_by = ARTICLE_PAGINATION_SIZE
 
 
 class ArticleDetailView(DetailView):
