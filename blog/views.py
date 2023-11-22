06ca6343b73340f235e01d9d84fe7505b86257c0
Anton Balabanov <asbbnv@gmail.com>
2023-11-22 10:06:28 +0200

    refactor ContactFormView: replace ContactRequest ModelForm with CreateView

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
+from django.conf import settings
+from django.core.mail import EmailMessage
+from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, FormView
+from django.views.generic import ListView, DetailView, CreateView
 
-from blog.forms import ContactForm
-from blog.models import Article
+from blog.models import Article, ContactRequest
 
 ARTICLE_PAGINATION_SIZE = 5
 
@@ -22,12 +24,27 @@ class ArticleDetailView(DetailView):
     queryset = Article.objects.filter(online=True)
 
 
-class ContactFormView(FormView):
-    form_class = ContactForm
-    template_name = 'blog/contact_form.html'
+class ContactFormView(CreateView):
+    model: ContactRequest = ContactRequest
+    fields: list[str, ] = ['name', 'email', 'content']
     success_url = reverse_lazy('blog-home')
 
-    def form_valid(self, form):
-        form.save()
-        form.send_email()
-        return super().form_valid(form)
+    def form_valid(self, form) -> HttpResponseRedirect:
+        """Save instance, send email, and only then redirect"""
+        result: HttpResponseRedirect = super().form_valid(form)
+        self.send_email(form)
+        return result
+
+    @staticmethod
+    def send_email(form) -> None:
+        name: str = form.cleaned_data['name']
+        email: str = form.cleaned_data['email']
+        content: str = form.cleaned_data['content']
+        email_message: EmailMessage = EmailMessage(
+            subject=f"New contact request from {name}",
+            body=content,
+            from_email=email,
+            to=[settings.DEFAULT_TO_EMAIL],
+            reply_to=[email]
+        )
+        email_message.send()
