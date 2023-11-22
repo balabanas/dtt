df9a37d095c21aa6a66f241421b77130c8a3a6eb
Anton Balabanov <asbbnv@gmail.com>
2023-11-22 09:08:42 +0200

    adjust naming in test_model_contact_request.py

--- blog/tests/test_model_contact_request.py
+++ blog/tests/test_model_contact_request.py
@@ -8,8 +8,8 @@ from blog.models import ContactRequest
 
 class ContactRequestTestCase(TestCase):
     def setUp(self) -> None:
-        self.cr: ContactRequest = ContactRequest.objects.create(name="My Name", email="my@test.com",
-                                                                content="test content")
+        self.contact_request: ContactRequest = ContactRequest.objects.create(name="My Name", email="my@test.com",
+                                                                             content="test content")
 
     def test_contact_request_retrieve(self) -> None:
         contact_request = ContactRequest.objects.get(name="My Name")
@@ -17,27 +17,29 @@ class ContactRequestTestCase(TestCase):
         self.assertEqual("my@test.com", contact_request.email)
 
     def test_contact_request_name_max_length(self) -> None:
-        cr: ContactRequest = ContactRequest(name="My Name" * 100, email="my@test.com", content="test content")
+        contact_request: ContactRequest = ContactRequest(name="My Name" * 100, email="my@test.com",
+                                                         content="test content")
         with self.assertRaises(ValidationError) as error:
-            cr.full_clean()
+            contact_request.full_clean()
         msg: str = error.exception.message_dict['name'][0]
         self.assertTrue(all(word in msg for word in ['at most', 'characters']))
 
     def test_contact_request_email_required(self) -> None:
-        cr: ContactRequest = ContactRequest(name="My Name", content="test content")
+        contact_request: ContactRequest = ContactRequest(name="My Name", content="test content")
         with self.assertRaises(ValidationError) as error:
-            cr.full_clean()
+            contact_request.full_clean()
         msg: str = error.exception.message_dict['email'][0]
         self.assertTrue(all(word in msg for word in ['cannot', 'blank']))
 
     def test_contact_request_email_validation(self) -> None:
-        cr: ContactRequest = ContactRequest(name="My Name", email="mytest.com", content="test content")
+        contact_request: ContactRequest = ContactRequest(name="My Name", email="mytest.com", content="test content")
         with self.assertRaises(ValidationError) as error:
-            cr.full_clean()
+            contact_request.full_clean()
         msg: str = error.exception.message_dict['email'][0]
         self.assertTrue(all(word in msg for word in ['valid', 'email']))
 
     def test_contact_request_pub_dttm_order(self) -> None:
-        time.sleep(0.01)  # wait 0.01 second to make sure the pub_dttm is different from self.cr
-        cr = ContactRequest.objects.create(name="My Name", email="my@test.com", content="test content")
-        self.assertGreater(cr.pub_dttm, self.cr.pub_dttm)
+        time.sleep(0.01)  # wait 0.01 second to make sure the pub_dttm is different from self.contact_request
+        contact_request: ContactRequest = ContactRequest.objects.create(name="My Name", email="my@test.com",
+                                                                        content="test content")
+        self.assertGreater(contact_request.pub_dttm, self.contact_request.pub_dttm)
